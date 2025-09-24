08628508ff55f2b01b5e00aa3fd13b90796ff04b
hwanykkk <olrkyoung@gmail.com>
2025-09-24 15:37:09 +0900

    250924

--- SaveGreen_Spring/src/main/java/com/example/co2/controller/SimulatorController.java
+++ SaveGreen_Spring/src/main/java/com/example/co2/controller/SimulatorController.java
@@ -1,6 +1,13 @@
 package com.example.co2.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class SimulatorController {
-    
+    @GetMapping("/simulator")
+    public String simulator() {
+        return "html/simulator"; // templates/simulator.html 렌더링
+    }
+
 }
