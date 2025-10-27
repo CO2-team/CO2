2fe4703d85d08c79ee199079442f0d1976651fc3
yunkwanghun <denidaschool@gmail.com>
2025-10-27 17:14:47 +0900

    update

--- SaveGreen_Spring/src/main/resources/static/js/forecast.kpi.js
+++ SaveGreen_Spring/src/main/resources/static/js/forecast.kpi.js
@@ -124,8 +124,8 @@
         else if (savingPct >= 10) score += 1;
 
         // 2. 회수기간
-        if (payback <= 5) score += 2;
-        else if (payback <= 8) score += 1;
+        if (payback <= 8) score += 2;
+        else if (payback <= 12) score += 1;
 
         // 3. 연식(없으면 중립 1점)
         let agePt = 1;
